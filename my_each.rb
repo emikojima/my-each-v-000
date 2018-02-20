e0f1cce9dc6e967340bbf0c5ab27001770061c7b
emi kojima <emi8emi@gmail.com>
2018-02-20 18:27:44 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,4 +1,5 @@
 def my_each(array)
+<<<<<<< HEAD
   count = 0
     while array.count != count
     yield array[count]
@@ -9,3 +10,18 @@ end
   
 
  
+=======
+  new_array = []
+  yield if block_given?
+    count = 0
+    while array.count != count
+    array.collect do |element|  new_array << element
+    count += 1
+    end
+  end
+  new_array
+  else
+  array
+  end  
+  
+>>>>>>> cea3a51a61a051149ed3536ebd2a908ac4dfa484
